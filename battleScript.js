a63681e9847a5644331cc1decc5d45f613f56a63
Ryusei Endo <riyu02180218@gmail.com>
2025-12-20 20:24:48 +0900

    煙エフェクト管制

--- battleScript.js
+++ battleScript.js
@@ -8,27 +8,41 @@ let hp1 = 120
 let hp2 = 100
 let at1 = 1
 let at2 = 1
+let kw = 0 //煙のエフェクトの幅
+let kh = 0 //煙のエフェクトの高さ
+let frame = 0
+let maisuu = 0
+let hitEffect = false;
 const backGround = new Image()
 backGround.src = "img/haikei.png";
 const characterChip = new Image()
 characterChip.src = "img/characterChip.png";
+const kemuriChip = new Image()
+kemuriChip.src = "img/kemuriChip.png";
 characterChip.onload = function(){
     animation()
 }
+
 function animation(){
     ctx.clearRect(0,0,instantCanvas.width,instantCanvas.height)
     ctx.drawImage(backGround,0,0)
-    if (x1 < x2 + 100 && x1 + 100 > x2){
+    frame += 1
+    if (x1+30 < x2 + 100 && x1 + 100 > x2){
         console.log("当たり判定")
         hp1 -= at1
         hp2 -= at2
-        gifler("img/at_effect.gif").frames(instantCanvas, (ctx, frame) => {
-            ctx.drawImage(
-                frame.buffer,
-                x1, y1-50,   // x, y
-                64, 64      
-            );
-        });
+
+        if (frame>30){
+            frame = 0
+            maisuu += 1
+            kw = 200*maisuu
+            if(maisuu > 7){
+               maisuu = 0 
+            }
+        }
+        ctx.drawImage(kemuriChip,0,0,200,200,x1-20,y1+40,50,50)
+        ctx.drawImage(kemuriChip,kw,kh,kw+200,kh+200,x1-20,y1+40,50,50)
+        console.log(kw,kh)
     }else{
         x1 -= 0.3
         x2 += 0.3
